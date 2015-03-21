ac30760615b0be5623618793fd73f763b055b766
Michael Bowman <bowmanmc@gmail.com>
2015-03-20 22:28:17 -0400

    Got headroom.js working with my navigation bar

--- app/scripts/navigation.js
+++ app/scripts/navigation.js
@@ -1,10 +1,17 @@
 $(function() {
-
     // Setup the site navigation
-    console.log('Setting up menu navigation');
     $('#menu-btn').click(function() {
         console.log('#menu-btn clicked!');
         $('#menu-btn').toggleClass('close');
     });
 
+    $("#navigation").headroom({
+        "offset": 70,
+        "tolerance": 2,
+        "classes": {
+            "initial": "animated",
+            "pinned": "slideDown",
+            "unpinned": "slideUp"
+        }
+    }).init();
 });
